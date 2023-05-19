93f67e71c3b7311902988ddaf34410878f3bc644
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-19 11:29:08 -0700

    submit --> local storage

--- newprofile/newprofile.js
+++ newprofile/newprofile.js
@@ -1,9 +1,9 @@
 /* show what they chose on the select box */
 function showSelectedOption(selectedOption) {
-    var selectBox = document.getElementById("status");
-    var selectedText = selectBox.options[selectBox.selectedIndex].text;
-    selectBox.options[selectBox.selectedIndex].innerHTML = selectedText + " (selected)";
-  }
+  var selectBox = document.getElementById("status");
+  var selectedOption = selectBox.options[selectBox.selectedIndex].text;
+  selectBox.innerHTML = '<option value="' + status + '" selected>' + selectedOption + '</option>';
+}
 
   function validateNumberInput(input) {
     var value = input.value;
@@ -14,4 +14,30 @@ function showSelectedOption(selectedOption) {
     // Update the input value with the cleaned value
     input.value = cleanedValue;
   }
+
+  // Function to handle form submission
+function handleSubmit() {
+  // Get input values
+  var name = document.getElementById("name").value;
+  var year = document.getElementById("year").value;
+  var date = document.getElementById("date").value;
+  var month = document.getElementById("month").value;
+  var status = document.getElementById("status").value;
+
+  // Create an object to store the form data
+  var formData = {
+    name: name,
+    year: year,
+    date: date,
+    month: month,
+    status: status
+  };
+
+  // Store the form data in local storage
+  localStorage.setItem("formData", JSON.stringify(formData));
+}
+
+// Attach event listener to the submit button
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", handleSubmit);
   
