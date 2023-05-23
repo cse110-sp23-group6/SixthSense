b59159eec306e43be44db65304ad3d49c50151f4
HyperBlitzer <90485689+HyperBlitzer@users.noreply.github.com>
2023-05-23 16:48:18 -0700

    Form Validation + Navigation
    - The submit button begins disabled
    - Once all 5 inputs [Name, Day, Month, Year, Relationship Status] are correctly filled, the submit button gets enabled
    close #32
    - Traversal to the welcome page added
    fix #35

--- assets/scripts/newprofile.js
+++ assets/scripts/newprofile.js
@@ -1,31 +1,63 @@
-/* show what they chose on the select box */
-function showSelectedOption(selectedOption) {
-  var selectBox = document.getElementById("status");
-  var selectedOption = selectBox.options[selectBox.selectedIndex].text;
-  selectBox.innerHTML = '<option value="' + status + '" selected>' + selectedOption + '</option>';
-}
+// Document Selectors
+let yearBox = document.getElementById('year');
+let dateBox = document.getElementById('date');
+let monthBox = document.getElementById('month');
+let nameBox = document.getElementById('name');
+let statusBox = document.getElementById('status');
+let submitButton = document.getElementById('submit');
+let homeButton = document.getElementById('home');
+// Start off disabled to not allow empty input
+submitButton.disabled = true;
 
-  function validateNumberInput(input) {
-    var value = input.value;
-    
-    // Remove any non-digit characters from the input value
-    var cleanedValue = value.replace(/\D/g, '');
-    
-    // Update the input value with the cleaned value
-    input.value = cleanedValue;
+/**
+ * Function that checks if the user correctly typed the right input. If not the
+ * submit button will remain disabled. When all 5 input types are satisfied,
+ * the submit button gets enabled. 
+ * 
+ * @param {event} e - the event that triggered the function call
+ */
+function validateInput(event) {
+  let numbers = /^[0-9]/;
+  let letters = /^[a-zA-Z\s]+$/;
+  let id = event.target.getAttribute('id');
+  let input = event.target.value;
+
+  // Check if Name input is not valid
+  if (id == 'name' && !input.match(letters)) {
+    submitButton.disabled = true;
+  }
+  
+  // Check if Birthday inputs are not valid
+  if (id != 'name' && id != 'status' && !input.match(numbers)) {
+    submitButton.disabled = true;
   }
 
-  // Function to handle form submission
+  let yearValid = yearBox.value.match(numbers);
+  let dateValid = dateBox.value.match(numbers);
+  let monthValid = monthBox.value.match(numbers);
+  let nameValid = nameBox.value.match(letters);
+  let statusValue = statusBox.value;
+
+  // If everything is inputed correctly, allow for submitting
+  if (yearValid && dateValid && monthValid && nameValid && statusValue != "") {
+    submitButton.disabled = false;
+  }
+}
+
+/**
+ * Function that stores the create profile information into JSON format to save.
+ * Also redirects the user to the welcome page.
+ */
 function handleSubmit() {
   // Get input values
-  var name = document.getElementById("name").value;
-  var year = document.getElementById("year").value;
-  var date = document.getElementById("date").value;
-  var month = document.getElementById("month").value;
-  var status = document.getElementById("status").value;
+  let name = nameBox.value;
+  let year = yearBox.value;
+  let date = dateBox.value;
+  let month = monthBox.value;
+  let status = statusBox.value;
 
   // Create an object to store the form data
-  var formData = {
+  let formData = {
     name: name,
     year: year,
     date: date,
@@ -35,9 +67,19 @@ function handleSubmit() {
 
   // Store the form data in local storage
   localStorage.setItem("formData", JSON.stringify(formData));
+  location.href = 'welcome-1.html';
 }
 
-// Attach event listener to the submit button
-var submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", handleSubmit);
+// Attach event listener to the buttons
+yearBox.addEventListener('change', validateInput);
+dateBox.addEventListener('change', validateInput);
+monthBox.addEventListener('change', validateInput);
+nameBox.addEventListener('change', validateInput);
+statusBox.addEventListener('change', validateInput);
+submitButton.addEventListener('click', handleSubmit);
+
+// Go back to welcome page if user clicks home
+homeButton.addEventListener('click', function () {
+  location.href = 'welcome-1.html';
+})
   
