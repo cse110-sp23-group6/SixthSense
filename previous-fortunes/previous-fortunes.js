10d8b20dd606bb4425d75144e61eafb05d982ed0
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-23 22:58:38 -0700

    add open button, also when selected, it glows

--- previous-fortunes/previous-fortunes.js
+++ previous-fortunes/previous-fortunes.js
@@ -1,55 +1,111 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Check if formatted readings exist in local storage
-  if (localStorage.getItem('formattedReadings')) {
-    // Retrieve the formatted readings from local storage
-    var formattedReadings = JSON.parse(localStorage.getItem('formattedReadings'));
-
-    // Get the <ul> element to populate
-    var ulElement = document.getElementById('readingList');
-
-    // Create an array to store the dynamically created <li> elements
-    var liElements = [];
-
-    // Loop through the formatted readings and create <li> elements
-    formattedReadings.forEach(function(reading) {
-      var liElement = document.createElement('li');
-      liElement.innerText = reading.date + ' - ' + reading.reading;
-      ulElement.appendChild(liElement);
-
-      // Store the dynamically created <li> elements in the array
-      liElements.push(liElement);
-    });
-
-    createEntries(liElements); // Move the function call here
-  }
-});
-
 function createEntries(listItems) {
-  const expandedContent = document.getElementById('expanded-content');
-
   for (let i = 0; i < listItems.length; i++) {
     listItems[i].addEventListener('click', function() {
-      const selectedItem = this.textContent;
-      expandedContent.textContent = selectedItem;
-      expandedContent.style.height = '0'; // Set initial height to 0
-      expandedContent.classList.add('expanded');
-
-      // Create and append the close button
-      const closeButton = document.createElement('button');
-      closeButton.textContent = 'Close';
-      closeButton.classList.add('close-button');
-      expandedContent.appendChild(closeButton);
-
-      // Add event listener to the close button
-      closeButton.addEventListener('click', function() {
-        expandedContent.classList.remove('expanded');
-        expandedContent.removeChild(closeButton); // Remove the close button
-      });
-
-      // Trigger the animation after a short delay to allow for height update
-      setTimeout(() => {
-        expandedContent.style.height = '65vh'; // Set the final expanded height
-      }, 10);
+      if (this.classList.contains('selected')) {
+        // If the item is already selected, remove the 'selected' class
+        this.classList.remove('selected');
+      } else {
+        // Remove 'selected' class from all items
+        for (let j = 0; j < listItems.length; j++) {
+          listItems[j].classList.remove('selected');
+        }
+        // Add 'selected' class to the clicked item
+        this.classList.add('selected');
+        console.log('selected:'+ this.innerText);
+      }
     });
-  }  
+  }
 }
+
+// Function to create the close button for the expanded content
+function createCloseButton() {
+  var closeButton = document.createElement('button');
+  closeButton.className = 'close-button';
+  closeButton.innerText = 'Close';
+  closeButton.addEventListener('click', closeExpandedContent);
+  return closeButton;
+}
+
+// Function to close the expanded content
+function closeExpandedContent() {
+  var expandedContent = document.getElementById('expanded-content');
+  expandedContent.classList.remove('expanded');
+}
+
+// Function to open the selected list item
+function openSelectedItem() {
+  var selectedItem = document.querySelector('.selected');
+  if (selectedItem) {
+    // Open the selected item (perform the desired action)
+    // For example:
+    console.log('Opening:', selectedItem.innerText);
+    var text = selectedItem.innerText;
+    
+    // Get the expanded content element
+    var expandedContent = document.getElementById('expanded-content');
+    
+    // Clear the content
+    expandedContent.innerHTML = text;
+    
+    // Add the close button to the expanded content
+    var closeButton = createCloseButton();
+    expandedContent.appendChild(closeButton);
+    
+    // Add the expanded class to show the expanded content
+    expandedContent.classList.add('expanded');
+  } else {
+    console.log('No item selected.');
+  }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Check if formatted readings exist in local storage
+  var formattedReadings = localStorage.getItem('formattedReadings')
+    ? JSON.parse(localStorage.getItem('formattedReadings'))
+    : [];
+
+  // Get the <ul> element to populate
+  var ulElement = document.getElementById('readingList');
+
+  // Create an array to store the dynamically created <li> elements
+  var liElements = [];
+
+  // Function to add a reading to the list
+  function addReadingToList(reading) {
+    var liElement = document.createElement('li');
+    liElement.innerText = reading.date + ' - ' + reading.reading;
+    ulElement.appendChild(liElement); // Append to the end
+    liElements.push(liElement);
+  }
+
+  // Check if there is a new reading in local storage
+  if (localStorage.getItem('newReading')) {
+    var newReading = JSON.parse(localStorage.getItem('newReading'));
+
+    // Check if the new reading has the correct properties
+    if (newReading.hasOwnProperty('date') && newReading.hasOwnProperty('reading')) {
+
+      // Add the new reading to the beginning of the formattedReadings array
+      formattedReadings.unshift(newReading);
+
+      // Remove the new reading from local storage
+      localStorage.removeItem('newReading');
+
+      // Update the formattedReadings in local storage
+      localStorage.setItem('formattedReadings', JSON.stringify(formattedReadings));
+    }
+  }
+  // Loop through the existing formatted readings and add them to the list in order
+  for (var i = 0; i < formattedReadings.length; i++) {
+    addReadingToList(formattedReadings[i]);
+    console.log(formattedReadings[i]);
+  }
+
+  createEntries(liElements); // Move the function call here
+
+  
+  var openButton = document.getElementById('openButton');
+  openButton.addEventListener('click', openSelectedItem);
+});
---
+++ previous-fortunes/testReadings.js
@@ -0,0 +1,50 @@
+//formattedReadings
+var formattedReadings = [
+  {
+    date: "May 17, 2023",
+    reading: "I sense that your love life may be filled with passion, excitement, and deep emotional connections with your partner. You may feel deeply in love and connected to your partner, and you may be experiencing a sense of joy and fulfillment in your relationship. Remember to cherish and nurture your feelings of ecstasy and stay open to new possibilities and experiences that come with it."
+  },
+  {
+    date: "May 14, 2023",
+    reading: "This may be a challenging time for you, but there is hope for the future. You may be experiencing some sadness or disappointment in your love life, perhaps because of a recent breakup or a feeling of being stuck in your current relationship. It's important to take time to process your emotions and to give yourself space to grieve if necessary. However, this melancholy phase may be a time of growth and introspection for you. It may be an opportunity to reflect on what you truly want and need in a relationship and to make changes or adjustments in your approach to love."
+  },
+  {
+    date: "May 3, 2023",
+    reading: "It's important to trust your instincts and to be open and receptive to new experiences and possibilities in your love life. It may be helpful to take some time to reflect on what you're looking for in a partner and what you want out of a relationship."
+  },
+  {
+    date: "Apr 20, 2023",
+    reading: "Sometimes we realize that we have been holding onto the past for too long. Although it will be a long and painful journey, you will come out on the other end happy and valiant."
+  },
+  {
+    date: "Apr 1, 2023",
+    reading: "Embrace introspection, seek guidance from mentors, and explore new connections. Trust in your abilities, persevere, and let the winds of determination guide you towards a fulfilling path."
+  },
+  {
+    date: "Mar 10, 2023",
+    reading: "Ah, curious soul, I sense a flicker of intrigue within you. Embrace this wondrous curiosity as a compass guiding you towards new horizons. Seek mentors who will nurture your passions, explore diverse paths, and let your inquisitive spirit shape a rewarding career that dances with the stars."
+  },
+  {
+    date: "Feb 1, 2023",
+    reading: "Embrace an unexpected twist as a catalyst for change. Unveil hidden talents, venture into uncharted territories, and let the surprise pave the way for a thrilling and transformative career path that exceeds even your wildest dreams."
+  },
+  {
+    date: "Feb 1, 2023",
+    reading: "Ah, noble spirit, let your sense of justice be your guiding light. Seek a career that aligns with your values, where your actions can bring positive change to the world. Embrace opportunities to advocate for what is right, for in the pursuit of a righteous path, you shall find fulfillment and leave an indelible mark upon the tapestry of life."
+  },
+  {
+    date: "Jan 27, 2023",
+    reading: "Release the chains that bind you, for within you lies untapped potential and boundless worth. Embrace self-compassion as your guiding star, seek mentors who illuminate your strengths, and embark on a journey of self-discovery. From the ashes of self-loathing, you shall rise, unveiling a career that honors your true essence and brings forth the light within."
+  }
+];
+
+localStorage.setItem('formattedReadings', JSON.stringify(formattedReadings));
+
+//newReading
+var newReading = {
+    date: "May 18 2023",
+    reading: "This is a test to check if my check for new reading and then add it to the formattedReadings list as well as adding it to the list on the screen works. If this doesn't work I'm going to literally cry. Hopefully this works and I don't have to worry about this stuff no more."
+};
+  
+
+localStorage.setItem('newReading', JSON.stringify(newReading));
