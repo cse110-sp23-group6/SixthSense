373ee24828757dfce76d5ba7a3aad30849dcb183
prishaanand <60487925+prishaanand@users.noreply.github.com>
2023-05-24 19:04:12 -0700

    delete semicolon

--- assets/scripts/emotions1.js
+++ assets/scripts/emotions1.js
@@ -136,7 +136,7 @@ function imageSet (int, button, index) {
 function shuffle (arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+    [arr[i], arr[j]] = [arr[j], arr[i]]
   }
-  return arr;
+  return arr
 }
