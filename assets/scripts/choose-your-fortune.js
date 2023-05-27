35c9667741553a16610a920b790d34b334d816fa
BZhang-ucsd <97600878+BZhang-ucsd@users.noreply.github.com>
2023-05-26 19:15:09 -0700

    Update choose-your-fortune.js fix jquery

--- assets/scripts/choose-your-fortune.js
+++ assets/scripts/choose-your-fortune.js
@@ -1,25 +1,28 @@
 const SECONDS_PER_DAY = 86400
 
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function() {
   const headerText = $('#header-text')
   const text = headerText.text()
   let html = ''
 
   for (let i = 0; i < text.length; i++) {
     if (text[i] === ' ') {
-      html += '<span class="header-hidden">&nbsp;</span>'
+      html += '<span class="header-hidden">&nbsp;</span>';
     } else {
-      html += `<span class="header-hidden">${text[i]}</span>`
+      html += `<span class="header-hidden">${text[i]}</span>`;
     }
   }
 
   headerText.html(html)
 
   let delay = 100 // time in milliseconds between each character
-  $('.header-hidden').each(function () {
-    $(this).delay(delay).animate({ opacity: 1 }, 200)
-    delay += 100
-  })
+  document.querySelectorAll('.header-hidden').forEach(function(elem) {
+    setTimeout(function() {
+    elem.style.opacity = 1;
+        }, delay);
+    delay += 100;
+    });
+});
 
   const loveDiv = document.getElementById('love-div')
   loveDiv.addEventListener('click', function () {
