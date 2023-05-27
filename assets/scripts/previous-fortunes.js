bf0f435b04d903b5dbf8887386e8012f0b875bc0
Haven Ahn <havenahn@Havens-MacBook-Air-1188.local>
2023-05-26 17:22:07 -0700

    profile

--- assets/scripts/previous-fortunes.js
+++ assets/scripts/previous-fortunes.js
@@ -1,10 +1,62 @@
 /*
  * File Name: previous-fortunes.js
  */
+import { EMOTIONS_TABLE, EMOTIONS } from "./constants.js";
 //create variable formattedReadings which pulls from local storage. used throughout the page
 let formattedReadings = localStorage.getItem('formattedReadings')
   ? JSON.parse(localStorage.getItem('formattedReadings'))
   : [];
+/*
+ * function name: getStarSign
+ * purpose: take in month and date and return the starsign
+ * 
+ * @input month: month of the birthday
+ * @input date: date of the birthday
+ * @param starSigns : starsigns and their dates
+ * @param monthNum, dateNum: parsed number version of month and date
+ * @param sign, startMonth, startDate, endMonth, endDate: [][] array to find starsign
+ * @return sign.name: name of the starsign
+ * @return unknown: in case there's nothing
+ */
+function getStarSign(month, date) {
+  // Define the date ranges for each star sign
+  let starSigns = [
+    { name: 'Aquarius', start: [1, 20], end: [2, 18] },
+    { name: 'Pisces', start: [2, 19], end: [3, 20] },
+    { name: 'Aries', start: [3, 21], end: [4, 19] },
+    { name: 'Taurus', start: [4, 20], end: [5, 20] },
+    { name: 'Gemini', start: [5, 21], end: [6, 20] },
+    { name: 'Cancer', start: [6, 21], end: [7, 22] },
+    { name: 'Leo', start: [7, 23], end: [8, 22] },
+    { name: 'Virgo', start: [8, 23], end: [9, 22] },
+    { name: 'Libra', start: [9, 23], end: [10, 22] },
+    { name: 'Scorpio', start: [10, 23], end: [11, 21] },
+    { name: 'Sagittarius', start: [11, 22], end: [12, 21] },
+    { name: 'Capricorn', start: [12, 22], end: [12, 31] },
+    { name: 'Capricorn', start: [1, 1], end: [1, 19] }
+  ];
+
+  // Convert the month and date strings to numbers
+  let monthNum = parseInt(month);
+  let dateNum = parseInt(date);
+
+  // Find the corresponding star sign
+  for (var i = 0; i < starSigns.length; i++) {
+    let sign = starSigns[i];
+    let startMonth = sign.start[0];
+    let startDate = sign.start[1];
+    let endMonth = sign.end[0];
+    let endDate = sign.end[1];
+
+    if (
+      (monthNum === startMonth && dateNum >= startDate) ||
+      (monthNum === endMonth && dateNum <= endDate)
+    ) {
+      return sign.name;
+    }
+  }
+  return 'Unknown';
+}
 /* 
  * function name: createEntries
  * create list entries(previous readings) and allows it to be selected
@@ -98,24 +150,34 @@ function deleteSelectedItem() {
 }
 //runs when opened
 document.addEventListener('DOMContentLoaded', function() {
-  //profile stuff
-  // Retrieve the form data from local storage
-  const storedData = localStorage.getItem('formData');
+  //profile emotion -- retrieve emotions from local storage
+  let emotion1 = localStorage.getItem('emotion1');
+  let emotion2 = localStorage.getItem('emotion2');
+  emotion1 = JSON.parse(emotion1);
+  emotion2 = JSON.parse(emotion2).emotion;
+  let overallEmotion = EMOTIONS_TABLE[emotion1][emotion2];
+  let finalemotion = document.getElementById('finalemotion');
+  finalemotion.alt = overallEmotion;
+  finalemotion.src = `assets/emotion_auras/${overallEmotion}.gif`;
+  // profile text -- Retrieve the form data from local storage
+  let formData = localStorage.getItem('formData');
   // Check if data exists in local storage
-  if (storedData) {
+  if (formData) {
     // Parse the JSON string back into an object
-    const formData = JSON.parse(storedData);
-    // Access the individual form fields
-    const name = formData.name;
-    const year = formData.year;
-    const date = formData.date;
-    const month = formData.month;
-    const status = formData.status;
-  console.log(name, year, date, month, status);
+    formData = JSON.parse(formData);
+  console.log(formData);
   } else {
     // Handle the case where no data is stored
     console.log('No form data found.');
   }
+  console.log(formData.name);
+  //set up name, star-sign, and relationship
+  let nameElement = document.getElementById('name');
+  nameElement.textContent = formData.name;
+  let signElement = document.getElementById('star-sign');
+  signElement.textContent = getStarSign(formData.month, formData.date);
+  let loveElement = document.getElementById('relationship');
+  loveElement.textContent = formData.status;
   // Get the <ul> element to populate
   let ulElement = document.getElementById('readingList');
   // Create an array to store the dynamically created <li> elements
