160c28074f0653b4a4157f41549dd368e05a536e
vbogdev <vbogdev@ucsd.edu>
2023-06-03 07:58:01 -0700

    Added tests for all categories

--- __tests__/E2E/e2e.test.js
+++ __tests__/E2E/e2e.test.js
@@ -15,6 +15,18 @@ describe('End to end testing', () => {
     expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
   });
 
+
+
+/**
+ * 
+ * Below are a bunch of test that tests going between selection pages
+ * 
+ */
+
+
+
+//----------------------------Love----------------------------
+
   it('choose your fortune to emotions1 (love) navigation test', async () => {
     let but1 = await page.$('#love-div');
     await but1.click();
@@ -70,4 +82,187 @@ describe('End to end testing', () => {
     let newUrl = await page.url();
     expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
   });
+
+  //----------------------------Career----------------------------
+
+  it('choose your fortune to emotions1 (career) navigation test', async () => {
+    let but1 = await page.$('#career-div');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=career');
+  });
+
+  it('emotions1 (career) select nothing test', async () => {
+    let but1 = await page.$('#button-right');
+    await but1.click();
+    await page.waitForTimeout(1000);
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=career');
+
+    let buttons = await page.$$('.round-button');
+    expect(buttons.length).toBe(5);
+  })
+
+  it('emotions1 (career) select something test', async () => {
+    let but1 = await page.$('#two');
+    await but1.click();
+    let but2 = await page.$('#button-right');
+    await but2.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions2.html?reading=career');
+  })
+
+  it('emotions2 (career) select nothing test', async () => {
+    let but1 = await page.$('#next');
+    await but1.click();
+    await page.waitForTimeout(1000);
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions2.html?reading=career');
+
+    let buttons = await page.$$('.button');
+    expect(buttons.length).toBe(5);
+  })
+
+  it('emotions2 to emotions1 (career)', async () => {
+    let but1 = await page.$('#back');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=career');
+  });
+
+  it('emotions1 (career) back to choose your fortune', async () => {
+    let but1 = await page.$('#button-left');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
+  });
+
+
+  
+  //----------------------------Health---------------------------
+
+  it('choose your fortune to emotions1 (health) navigation test', async () => {
+    let but1 = await page.$('#health-div');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=health');
+  });
+
+  it('emotions1 (health) select nothing test', async () => {
+    let but1 = await page.$('#button-right');
+    await but1.click();
+    await page.waitForTimeout(1000);
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=health');
+
+    let buttons = await page.$$('.round-button');
+    expect(buttons.length).toBe(5);
+  })
+
+  it('emotions1 (health) select something test', async () => {
+    let but1 = await page.$('#two');
+    await but1.click();
+    let but2 = await page.$('#button-right');
+    await but2.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions2.html?reading=health');
+  })
+
+  it('emotions2 (health) select nothing test', async () => {
+    let but1 = await page.$('#next');
+    await but1.click();
+    await page.waitForTimeout(1000);
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions2.html?reading=health');
+
+    let buttons = await page.$$('.button');
+    expect(buttons.length).toBe(5);
+  })
+
+  it('emotions2 to emotions1 (health)', async () => {
+    let but1 = await page.$('#back');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=health');
+  });
+
+  it('emotions1 (health) back to choose your fortune', async () => {
+    let but1 = await page.$('#button-left');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
+  });
+
+  //----------------------------Friends/Family---------------------------
+
+  it('choose your fortune to emotions1 (friends_and_family) navigation test', async () => {
+    let but1 = await page.$('#health-div');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=friends_and_family');
+  });
+
+  it('emotions1 (friends_and_family) select nothing test', async () => {
+    let but1 = await page.$('#button-right');
+    await but1.click();
+    await page.waitForTimeout(1000);
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=friends_and_family');
+
+    let buttons = await page.$$('.round-button');
+    expect(buttons.length).toBe(5);
+  })
+
+  it('emotions1 (friends_and_family) select something test', async () => {
+    let but1 = await page.$('#two');
+    await but1.click();
+    let but2 = await page.$('#button-right');
+    await but2.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions2.html?reading=friends_and_family');
+  })
+
+  it('emotions2 (friends_and_family) select nothing test', async () => {
+    let but1 = await page.$('#next');
+    await but1.click();
+    await page.waitForTimeout(1000);
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions2.html?reading=friends_and_family');
+
+    let buttons = await page.$$('.button');
+    expect(buttons.length).toBe(5);
+  })
+
+  it('emotions2 to emotions1 (friends_and_family)', async () => {
+    let but1 = await page.$('#back');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/emotions1.html?reading=friends_and_family');
+  });
+
+  it('emotions1 (friends_and_family) back to choose your fortune', async () => {
+    let but1 = await page.$('#button-left');
+    await but1.click();
+    await page.waitForNavigation();
+    let newUrl = await page.url();
+    expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
+  });
 });
+
+
+/**
+ * 
+ * Below are a bunch of tests that check the general readings page
+ * 
+ */
