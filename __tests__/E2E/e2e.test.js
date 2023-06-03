2e4806fba3cbff53fcf045f45b6499dde9efcb38
vbogdev <vbogdev@ucsd.edu>
2023-06-03 06:44:57 -0700

    added choose your fortune to emotions1-love test

--- __tests__/E2E/e2e.test.js
+++ __tests__/E2E/e2e.test.js
@@ -15,13 +15,11 @@ describe('End to end testing', () => {
         expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
     });
 
-    /*it('choose your fortune to choose a picture (love) navigation test', async () => {
-        let but1 = await page.$('#b1');
-        //let text = await page.evaluate(but => but.textContent, but1);
-        //console.log(text);
+    it('choose your fortune to choose a picture (love) navigation test', async () => {
+        let but1 = await page.$('#love-div');
         await but1.click();
         await page.waitForNavigation();
         let newUrl = await page.url();
-        expect(newUrl).toBe('http://127.0.0.1:8080/choose-your-fortune.html');
-    });*/
+        expect(newUrl).toBe('http://127.0.0.1:5500/emotions1.html?reading=love');
+    });
   });
