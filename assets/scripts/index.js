a5d33853a281d2022c59ed4afe575e1a17c4d563
Brian Ton <mrtonbrian@gmail.com>
2023-06-09 00:34:08 -0700

    Add VolumeControl component to other pages

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,77 +4,66 @@
  * they can either go to new fortunes, create new users, or previous fortunes
  * if they already have a profile, the new user button will not be availabel for them.
  * Previous fortunes will tell them that they have to have at least 1 fortune or a profile
- * 
- * @const userButton: user button
- * @const mainButtons: inner buttons including today and previous
- * @const todayButton: today's fortune button
- * @const previousButton: previous forutnes button
- * @const formData: profile data from newprofile.html
  */
 
-// Document Selectors
-const userButton = document.getElementById('user');
-const mainButtons = document.getElementsByClassName('inner-button');
-const todayButton = mainButtons[0];
-const previousButton = mainButtons[1];
+import { playButtonHoverSound } from "./VolumeControl.js";
 
-// Checks if the user has created a profile. If they have disable the button
-const formData = window.localStorage.getItem('formData');
-if (formData !== null) {
-  userButton.disabled = true;
-}
-/**
- * purpose: event listener for new user button
- * traverses to newprofile.html when clicked
- */
-userButton.addEventListener('click', function () {
-  location.href = 'newprofile.html';
-});
-/**
- * purpose: event listener for today's fortune button
- * traverses to choose-your-fortune.html when clicked
- */
-todayButton.addEventListener('click', function () {
-  location.href = 'choose-your-fortune.html';
-});
-/**
- * purpose: event listener for previous fortunes button
- * when clicked but doesn't have a profile, it tells you to create a profile
- * if you don't have a single fortune, it says you need at least 1 previously generated forutne
- * if user qualifies for both, it takes the user to previous-fortunes.html
- * 
- * @const readings: readings from local storage
- */
-previousButton.addEventListener('click', function () {
-  const readings = window.localStorage.getItem('readings');
-  if (formData === null) {
-    alert("Please create a profile to access previous fortunes")
-    return
+function init() {
+  // Document Selectors
+  const userButton = document.getElementById('user');
+  const mainButtons = document.getElementsByClassName('inner-button');
+  const todayButton = mainButtons[0];
+  const previousButton = mainButtons[1];
+
+  // Checks if the user has created a profile. If they have disable the button
+  const formData = window.localStorage.getItem('formData');
+  if (formData !== null) {
+    userButton.disabled = true;
   }
-  if (readings === null) {
-    alert("you need at least 1 previously generated fortune to access previous fortunes")
-    return
-  }
-  else {
-    location.href = 'previous-fortunes.html';
-  }
-});
+  /**
+   * purpose: event listener for new user button
+   * traverses to newprofile.html when clicked
+   */
+  userButton.addEventListener('click', function () {
+    location.href = 'newprofile.html';
+  });
+  /**
+   * purpose: event listener for today's fortune button
+   * traverses to choose-your-fortune.html when clicked
+   */
+  todayButton.addEventListener('click', function () {
+    location.href = 'choose-your-fortune.html';
+  });
+  /**
+   * purpose: event listener for previous fortunes button
+   * when clicked but doesn't have a profile, it tells you to create a profile
+   * if you don't have a single fortune, it says you need at least 1 previously generated forutne
+   * if user qualifies for both, it takes the user to previous-fortunes.html
+   * 
+   * @const readings: readings from local storage
+   */
+  previousButton.addEventListener('click', function () {
+    const readings = window.localStorage.getItem('readings');
+    if (formData === null) {
+      alert("Please create a profile to access previous fortunes")
+      return
+    }
+    if (readings === null) {
+      alert("you need at least 1 previously generated fortune to access previous fortunes")
+      return
+    }
+    else {
+      location.href = 'previous-fortunes.html';
+    }
+  });
 
+  const volumeControl = document.createElement("volume-control");
+  const soundButtonContainer = document.getElementById("sound-button-container");
+  soundButtonContainer.appendChild(volumeControl);
 
-/**
- * function name: playButtonHoverSound
- * purpose: Function to play the button hover sound
- * 
- * @const buttonHoverSound: new audio with soundSrc
- */
-function playButtonHoverSound() {
-  const volumeControl = document.querySelector("volume-control");
-  const buttonHoverSound = new Audio("assets/sounds/button-hover.mp3");
-  buttonHoverSound.volume = (volumeControl.getAttribute("value")) / 20; // change volume according sound bar
-  buttonHoverSound.currentTime = 0; // Reset the sound to start
-  buttonHoverSound.play();
+  todayButton.addEventListener('mouseenter', playButtonHoverSound);
+  previousButton.addEventListener('mouseenter', playButtonHoverSound);
+  userButton.addEventListener('mouseenter', playButtonHoverSound);
 }
 
-todayButton.addEventListener('mouseenter', playButtonHoverSound);
-previousButton.addEventListener('mouseenter', playButtonHoverSound);
-userButton.addEventListener('mouseenter', playButtonHoverSound);
+document.addEventListener('DOMContentLoaded', init);
--- assets/scripts/reading.js
+++ assets/scripts/reading.js
@@ -3,6 +3,7 @@
  * purpose: show emotion and reading based off of the category and emotions they chose
  */
 
+import { playButtonHoverSound } from './VolumeControl.js';
 import { READINGS, EMOTIONS_TABLE, READING_TYPES, RE_ASK_INTERVAL_SECONDS } from './constants.js';
 import { copyStringToClipboard, encodeSearchParams, randomInt } from './helpers.js';
 
@@ -138,8 +139,7 @@ async function init() {
     });
 
     localStorage.setItem('readings', JSON.stringify(currentReadings))
-  })
-
+  });
 
   // Attach click event listener to the share button
   const shareBtn = document.getElementById("share");
@@ -162,111 +162,17 @@ async function init() {
     alert("Share link copied to clipboard!");
   });
 
+  const volumeControl = document.createElement("volume-control");
+  const soundButtonContainer = document.getElementById("sound-button-container");
+  soundButtonContainer.appendChild(volumeControl);
 }
 
 window.addEventListener('DOMContentLoaded', init);
 
-// sound control
-const backgroundSound = new Audio('assets/sounds/background-music.mp3');
-const volumeSlider = document.getElementById('volume-slider');
-const volumeIcon = document.getElementById('volume-icon');
-
-let lastVolume = retrieveVolume(); // Retrieve volume value from local storage
-backgroundSound.volume = lastVolume; // Set the initial volume
-  // Set the volume slider to reflect the initial volume
-volumeSlider.value = lastVolume; 
-updateVolume();
-backgroundSound.currentTime = 0; // Reset the background sound to start
-backgroundSound.loop = true; // Enable looping
-backgroundSound.play(); // Play the background sound
-
-// mute and unmute for clicking icon
-
-function retrieveVolume() {
-  let storedVolume = localStorage.getItem('lastVolume');
-
-  // If there's no stored volume level or it's 0, set the default to 1
-  if (storedVolume === null) {
-    return 1;
-  } else {
-    return Number(storedVolume); // Convert the stored string value to a number before returning
-  }
-}
-
-// store the last volumn in record 
-volumeSlider.addEventListener('input', function () {
-  backgroundSound.volume = volumeSlider.value;
-  lastVolume = backgroundSound.volume; // Update the lastVolume to reflect the volume of the background sound
-  localStorage.setItem('lastVolume', lastVolume); // Store the volume level in local storage
-  updateVolume();
-});
-
-volumeIcon.addEventListener('click', function() {
-  if (backgroundSound.volume == 0) {
-    if (lastVolume == 0) {
-      backgroundSound.volume = 1;
-      volumeSlider.value = 1;
-    } else {
-      backgroundSound.volume = lastVolume;
-      volumeSlider.value = lastVolume;
-    }
-  } else {
-    lastVolume = backgroundSound.volume;
-    backgroundSound.volume = 0;
-    volumeSlider.value = 0;
-  }
-  updateVolume();
-});
-
-volumeSlider.addEventListener('change', updateVolume);
-volumeSlider.addEventListener('mousemove', updateVolume);
-
-/**
- * function name: updateVolume
- * purpose: update the volume of sound effects
- * 
- * @const volumelevel: level of volume ranges from 0 to 3
- */
-function updateVolume() {
-  backgroundSound.volume = volumeSlider.value;
-  backgroundSound.volume = volumeSlider.value;
-  let volumeLevel;
-
-  window.localStorage.setItem('lastVolume', volumeSlider.value);
-
-  if (backgroundSound.volume === 0) {
-    volumeLevel = "0";
-  } else if (backgroundSound.volume < 0.33) {
-    volumeLevel = "1";
-  } else if (backgroundSound.volume < 0.66) {
-    volumeLevel = "2";
-  } else {
-    volumeLevel = "3";
-  }
-  volumeIcon.src = `assets/images/volume-level-${volumeLevel}.svg`;
-}
-
-/**
- * function name: playButtonHoverSound
- * purpose: Function to play the button hover sound
- * 
- * @const buttonHoverSound: new audio with soundSrc
- */
-function playButtonHoverSound(soundSrc) {
-  const buttonHoverSound = new Audio(soundSrc);
-  buttonHoverSound.volume = (volumeSlider.value) / 20; // change volume according sound bar
-  buttonHoverSound.currentTime = 0; // Reset the sound to start
-  buttonHoverSound.play();
-}
-
 // back new fortune hover sounds
 const getNewFortune = document.getElementById('new-fortune');
-getNewFortune.addEventListener('mouseenter', function () {
-  playButtonHoverSound('assets/sounds/button-hover.mp3');
-});
+getNewFortune.addEventListener('mouseenter', playButtonHoverSound);
 
 // previous fortune hover sounds
 const previousButton = document.getElementById('create-profile');
-previousButton.addEventListener('mouseenter', function () {
-  playButtonHoverSound('assets/sounds/button-hover.mp3');
-});
+previousButton.addEventListener('mouseenter', playButtonHoverSound);
